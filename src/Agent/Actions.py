434b11eb7ab8c5431eedd8ef60124f3619d88764
TEK-s <82441072+TEK-s@users.noreply.github.com>
2022-10-27 20:39:09 -0400

    Create Actions.py

---
+++ src/Agent/Actions.py
@@ -0,0 +1,12 @@
+import enum
+
+
+class Actions(enum.Enum):
+
+    MOVE_UP = 'move_up'
+    MOVE_DOWN = 'move_down'
+    MOVE_LEFT = 'move_left'
+    MOVE_RIGHT = 'move_right'
+
+    VACUUM = 'vacuum'
+    COLLECT = 'collect'
